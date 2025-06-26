d18ac46252d4a44d2d819418e4c4370ab7db1d84
0PeterAdel <1peteradel@gmail.com>
2025-06-26 13:24:56 +0300

    fix errors

--- bot/bot_main.py
+++ bot/bot_main.py
@@ -1,16 +1,21 @@
-# bot/bot_main.py
+# bot/bot_main.py - FINAL CORRECTED IMPORTS
+
 import logging
 from telegram.ext import (
     Application, CommandHandler, ConversationHandler, MessageHandler, filters
 )
-from config import TELEGRAM_BOT_TOKEN
-# Import handlers from the new structure
-from handlers.registration import (
+
+# This is an ABSOLUTE import from the project root
+from config import TELEGRAM_BOT_TOKEN 
+# These are RELATIVE imports from within the 'bot' package
+from .handlers.registration import (
     start_command, login_start, received_username, received_password,
     set_language, received_language, cancel,
     USERNAME, PASSWORD, LANGUAGE
 )
-from handlers.info import balance_summary_command
+from .handlers.info import balance_summary_command
+# We will add more handlers here later
+# from .handlers.expenses import ...
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -47,7 +52,7 @@ def main() -> None:
     application.add_handler(lang_conv_handler)
     application.add_handler(CommandHandler("balance", balance_summary_command))
     
-    # ... Here you would add handlers for /new_expense, /my_votes, etc.
+    # ... Other handlers will be added here ...
     
     logger.info("Bot is running...")
     application.run_polling()
